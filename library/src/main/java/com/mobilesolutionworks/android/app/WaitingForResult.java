4f76c743a74b33fb34376f50b6e6ae99eea11ebc
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2017-03-08 00:42:54 +0800

    We don't need WaitingForResult as this child fragment onActivityResult had been fixed

--- library/src/main/java/com/mobilesolutionworks/android/app/WaitingForResult.java
+++
@@ -1,7 +0,0 @@
-package com.mobilesolutionworks.android.app;
-
-/**
- * Created by yunarta on 31/1/16.
- */
-public interface WaitingForResult {
-}
--- library/src/main/java/com/mobilesolutionworks/android/app/WorksActivity.java
+++ library/src/main/java/com/mobilesolutionworks/android/app/WorksActivity.java
@@ -19,7 +19,6 @@ public class WorksActivity extends AppCompatActivity {
     private WorksControllerManager mController;
 
     public WorksActivity() {
-//        mTrackInfoMap = new SparseArray<>();
     }
 
     @Override
@@ -62,81 +61,4 @@ public class WorksActivity extends AppCompatActivity {
         mController.dispatchSaveInstanceState(outState);
         super.onSaveInstanceState(outState);
     }
-
-    //    @Override
-//    public void startActivityFromFragment(Fragment fragment, Intent intent, int requestCode) {
-//        if (fragment instanceof WaitingForResult) {
-//            FragmentManager fm = fragment.getFragmentManager();
-//            int id = fm.getFragments().indexOf(fragment);
-//
-//            FragmentTrackInfo info = new FragmentTrackInfo(id);
-//
-//            Fragment parent = fragment;
-//            while ((parent = parent.getParentFragment()) != null) {
-//                fm = parent.getFragmentManager();
-//                id = fm.getFragments().indexOf(parent);
-//                info = new FragmentTrackInfo(id, info);
-//            }
-//
-//            mTrackInfoMap.put(requestCode, info);
-//            super.startActivityForResult(intent, requestCode);
-//        } else {
-//            super.startActivityFromFragment(fragment, intent, requestCode);
-//        }
-//    }
-//
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        int key = requestCode & 0xffff;
-//
-//        FragmentTrackInfo trackInfo = mTrackInfoMap.get(key);
-//        mTrackInfoMap.remove(key);
-//
-//        if (trackInfo != null) {
-//            FragmentManager fm = getSupportFragmentManager();
-//            Fragment fragment = fm.getFragments().get(trackInfo.mId);
-//
-//            if (trackInfo.mChild != null) {
-//                FragmentTrackInfo childInfo = trackInfo;
-//                Fragment child = fragment;
-//
-//                while ((childInfo = childInfo.mChild) != null) {
-//                    fm = child.getChildFragmentManager();
-//                    child = fm.getFragments().get(childInfo.mId);
-//                }
-//
-//                fragment = child;
-//            }
-//
-//            fragment.onActivityResult(requestCode, resultCode, data);
-//        } else {
-//            super.onActivityResult(requestCode, resultCode, data);
-//        }
-//    }
-//
-//    private class FragmentTrackInfo {
-//
-//        FragmentTrackInfo mChild;
-//
-//        int mId;
-//
-//        FragmentTrackInfo(int id) {
-//            mId = id;
-//        }
-//
-//        FragmentTrackInfo(int id, FragmentTrackInfo info) {
-//            mId = id;
-//            mChild = info;
-//        }
-//
-//        public int getId() {
-//            int requestId = 0;
-//            if (mChild != null) {
-//                requestId |= mChild.getId() << 8;
-//            }
-//
-//            requestId |= mId;
-//            return requestId;
-//        }
-//    }
 }
