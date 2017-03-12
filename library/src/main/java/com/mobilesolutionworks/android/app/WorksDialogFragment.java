2383b36dddca9c4fddae3f3af73a22f7b79c3873
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2017-03-12 20:59:24 +0800

    Dialog might not have create view function, move restore instance to create dialog instance

--- library/src/main/java/com/mobilesolutionworks/android/app/WorksDialogFragment.java
+++ library/src/main/java/com/mobilesolutionworks/android/app/WorksDialogFragment.java
@@ -1,7 +1,9 @@
 package com.mobilesolutionworks.android.app;
 
+import android.app.Dialog;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
@@ -35,10 +37,11 @@ public class WorksDialogFragment extends DialogFragment {
         mController.getLifecycleHook().onConfigurationChanged(newConfig);
     }
 
+    @NonNull
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
         mController.getLifecycleHook().onRestoreInstanceState(savedInstanceState);
+        return super.onCreateDialog(savedInstanceState);
     }
 
     @Override
