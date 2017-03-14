4c404961afe1aa63bd8ecf328864eae07bbbcd48
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2017-03-15 01:18:54 +0800

    Rename test package

---
+++ core/src/androidTest/mock/com/mobilesolutionworks/android/controller/test/GetController.java
@@ -0,0 +1,12 @@
+package com.mobilesolutionworks.android.controller.test;
+
+import com.mobilesolutionworks.android.app.controller.WorksController;
+
+/**
+ * Created by yunarta on 12/3/17.
+ */
+
+public interface GetController<C extends WorksController> {
+
+    C getController();
+}
