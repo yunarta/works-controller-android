f2eede795d14a4caccfbb312f6cb56aa074fe53d
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2017-03-27 14:38:11 +0800

    Develop (#9)
    * Update javadoc
    
    * Update javadoc
    
    * Fix @NonNull causing compile failure
    
    * Fix controller manager does not have context within onCreate() phase
    
    * Added demo app
    
    * Update sonar prop
    
    * Update gradle
    
    * Update based on sonar request
    
    * Update test
    
    * Revise code based on sonar
    
    * Added more demo and revisions
    
    * Adds more detailed demo
    
    * Add keystore
    
    * Remove test from samples
    
    * Simplifies gradle
    
    * Delete test
    
    * bintray publishing code added
    
    * Create README.md
    
    * Add function for travis to use company from environment variables
    
    * Exclude WorksDialogFragment and WorksFragment from duplication analysis
    
    * Ranem method from runOnUIWhenIsReady to runWhenUiIsReady
    
    * Merge contrib library into core
    
    * Update .travis.yml
    
    * Update core to 1.1.0
    
    * Update readm
    
    * Fix compile problem
    
    * SuppressWarnings("squid:S1604") cause we are not running on JDK 8
    
    * Revision based on sonar advise
    
    * Update scanner

--- run_scanner.sh
+++ run_scanner.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-sonar-scanner -Dsonar.login=$SONAR_TOKEN -Dsonar.projectVersion=$TRAVIS_BUILD_NUMBER -Dsonar.organization=$SONAR_ORGANIZATION
+sonar-scanner -Dsonar.login=$SONAR_TOKEN -Dsonar.projectVersion=$TRAVIS_BUILD_NUMBER -Dsonar.organization=$SONAR_ORGANIZATION -Dsonar.projectKey=$SONAR_PROJECT_KEY -Dsonar.host.url=https://sonarqube.com
