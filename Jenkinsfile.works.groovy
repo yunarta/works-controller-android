c90e11e1196454de7aa8501c640d3a8fefbd53da
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2018-06-11 01:25:35 +0800

    Modify upstream Jenkinsfile

--- Jenkinsfile.works.groovy
+++ Jenkinsfile.works.groovy
@@ -20,88 +20,51 @@ pipeline {
         }
 
         stage("Build") {
-            parallel {
-                stage("Test & Analyze") {
-                    when {
-                        not {
-                            branch "release/*"
-                        }
-                    }
-                    options {
-                        retry(2)
-                    }
-                    steps {
-                        echo "Build for test and analyze"
-                        sh '''./gradlew detektCheck -q'''
-                        androidEmulator command: "start", avd: "android-19"
-                        
-                        sh '''echo "Execute test"
+            when {
+                not {
+                    branch "release/*"
+                }
+            }
+
+            options {
+                retry(2)
+            }
+
+            steps {
+                echo "Build for test and analyze"
+                sh '''./gradlew detektCheck -q'''
+                androidEmulator command: "start", avd: "android-19"
+
+                sh '''echo "Execute test"
                         wget https://dl.bintray.com/linkedin/maven/com/linkedin/testbutler/test-butler-app/1.3.2/test-butler-app-1.3.2.apk -O test-butler-app.apk
                         $ANDROID_HOME/platform-tools/adb install -r test-butler-app.apk
                         ./gradlew cleanTest jacocoTestReport -PignoreFailures=true'''
-                    }
-                    post {
-                        always {
-                            androidEmulator command: "stop"
-                        }
-                    }
-                }
+            }
 
-                stage("Release") {
-                    options {
-                        retry(2)
-                    }
-                    when { branch 'release/*' }
-                    steps {
-                        echo "Build for release"
-
-                        sh './gradlew clean test worksCreatePublication -PignoreFailures=false'
-                    }
+            post {
+                always {
+                    androidEmulator command: "stop"
                 }
             }
         }
 
-        stage("Publish") {
-            parallel {
-                stage("Test & Analyze") {
-                    when {
-                        not {
-                            branch "release/*"
-                        }
-                    }
-                    steps {
-                        echo "Publishing test and analyze result"
-
-                        jacoco execPattern: 'build/jacoco/*.exec', classPattern: 'library/build/tmp/kotlin-classes/debug', sourcePattern: ''
-                        junit allowEmptyResults: true, testResults: '**/androidTest-results/connected/**/*.xml,**/test-results/**/*.xml'
-                        checkstyle canComputeNew: false, defaultEncoding: '', healthy: '', pattern: '**/detekt-report.xml', unHealthy: ''
-
-                        codeCoverage()
-                    }
-                }
-
-                stage("Release") {
-                    when { branch 'release/*' }
-                    steps {
-                        echo "Publishing release"
-
-                        // publish()
-                    }
+        stage("Publish Test & Analyze") {
+            when {
+                not {
+                    branch "release/*"
                 }
             }
-        }
-    }
-}
 
-def publish() {
-    def who = env.JENKINS_WHO ?: "anon"
-    if (who == "works") {
-        bintrayPublish([
-                credential: "mobilesolutionworks.jfrog.org",
-                pkg       : readJSON(file: 'works-publish/module.json'),
-                repo      : "mobilesolutionworks/release",
-                src       : "works-publish/build/libs"
-        ])
+            steps {
+                echo "Publishing test and analyze result"
+
+                jacoco execPattern: 'build/jacoco/*.exec', classPattern: 'library/build/tmp/kotlin-classes/debug', sourcePattern: ''
+                junit allowEmptyResults: true, testResults: '**/androidTest-results/connected/**/*.xml,**/test-results/**/*.xml'
+                checkstyle canComputeNew: false, defaultEncoding: '', healthy: '', pattern: '**/detekt-report.xml', unHealthy: ''
+
+                codeCoverage()
+            }
+        }
     }
 }
 
